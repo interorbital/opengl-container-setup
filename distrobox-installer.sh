a44d06c20db999d6eefaf231145e0aeaa9c67fea
interorbital <106621171+interorbital@users.noreply.github.com>
2022-06-01 18:09:47 +0300

    Update distrobox-installer.sh

--- distrobox-installer.sh
+++ distrobox-installer.sh
@@ -1,8 +1,3 @@
 sudo apt install podman git curl
-
-get_distrobox()
-{
-    curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
-}
-
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
 get_distrobox;
